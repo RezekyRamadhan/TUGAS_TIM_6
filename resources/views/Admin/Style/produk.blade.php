103374d3dfa238dd72c378bd9813f9b4243c3fed
Dekna <kdkrisnaaa@gmail.com>
2023-08-19 22:17:48 +0800

    Test push bang

--- resources/views/Admin/Style/produk.blade.php
+++ resources/views/Admin/Style/produk.blade.php
@@ -12,7 +12,7 @@
                     <h5 class="text-primary mb-0">$199.00</h5>
                     <div class="btn-action d-flex justify-content-center">
                         <a class="btn btn-primary py-2 px-3" href=""><i class="bi bi-cart"></i></a>
-                        <a class="btn btn-primary py-2 px-3" href="/produk{{ $user->id }}"><i class="bi bi-eye"></i></a>
+                        {{-- <a class="btn btn-primary py-2 px-3" href="/produk{{ $user->id }}"><i class="bi bi-eye"></i></a> --}}
                     </div>
                 </div>
             </div>
